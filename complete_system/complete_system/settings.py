9861e99e04ed5473974a74aa976feec0e6749e52
Muhammad Usama <51862131+Muhammad-Usama-07@users.noreply.github.com>
2023-09-21 22:58:52 +0500

    adding templates folder to the setting.py of the project
    add templates like this:
    TEMPLATES = [
        {
            'BACKEND': 'django.template.backends.django.DjangoTemplates',
            'DIRS': [os.path.join(BASE_DIR, 'templates')],
            # ...
        },
    ]

--- complete_system/complete_system/settings.py
+++ complete_system/complete_system/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -53,8 +53,8 @@ ROOT_URLCONF = "complete_system.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
